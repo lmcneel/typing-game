bd4d95c63fbc02f8130b4a6bb985084f01053f01
Latisha McNeel <latisha.mcneel@gmail.com>
2020-04-25 18:22:23 -0500

    Timer working

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,40 +3,63 @@ const text2 = 'Hello world!';
 const textEl = document.querySelector('#typing-text');
 textEl.textContent = text;
 let onLetter = 0; // TODO: Rename this variable
+const interval = 1000;
+const totalTime = 5;
+let remainingTime = totalTime;
+let letterCount = 0;
+const timerEl = document.querySelector('#timer');
+timerEl.textContent = `${remainingTime} seconds`;
+let timer;
+// 1 second = 1000 ms ->
+// 30 seconds * 1000 ms = 30000 ms
+// Keep count
+// Set a timer
+
+// formula for WPM = typedCharacters/5/Time -> 30 second
+
+function triggerTimer() {
+    console.log('TRIGGER TIMER');
+    timer = setInterval(function countDown() {
+        remainingTime--;
+        timerEl.textContent = `${remainingTime} seconds`;
+        if (remainingTime <= 0) gameOver();
+    }, interval);
+}
+
+function gameOver() {
+    clearInterval(timer);
+    timer = undefined;
+    remainingTime = totalTime;
+    console.log('GAME OVER');
+}
 
 document.body.addEventListener('keyup', (event) => {
-    console.group('KEY UP');
-    console.log('EVENT KEY', event.key);
-    console.log('TEXT LETTER', text[onLetter]);
-    if (event.key === text[onLetter]) {
-        console.log('SUCCESS');
-        onLetter++;
-        // TODO: Progress the visual letter cue
-        // Change letter to blue
-        // match the border
-        // Change the text from the 1st letter to the current onLetter
-        // ADDING HTML so I need to use the innerHTML function
-        // slice(0, onLetter) -> Should be the part that needs to wrapped
-        // slice(onLetter) -> this should the part that shouldn't be wrapped
-        const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
-            0,
-            onLetter
-        )}</span>`;
-        const staticText = text.slice(onLetter);
-        textEl.innerHTML = goodText + staticText;
-    } else {
-        // TODO: Indicate to the user that the letter is wrong
-        // TODO: Stretch Goal::: ADD AUDITORY CLUE
-        // change letter to red
-        // match the border
-        console.log('FAILURE');
-        const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
-            0,
-            onLetter
-        )}</span>`;
-        const badText = `<span class="text-danger border-bottom border-danger">${text[onLetter]}</span>`;
-        const staticText = text.slice(onLetter + 1);
-        textEl.innerHTML = goodText + badText + staticText;
+    if (!timer) triggerTimer();
+    if (remainingTime >= 0) {
+        letterCount++;
+        console.group('KEY UP');
+        console.log('EVENT KEY', event.key);
+        console.log('TEXT LETTER', text[onLetter]);
+        if (event.key === text[onLetter]) {
+            console.log('SUCCESS');
+            onLetter++;
+            const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
+                0,
+                onLetter
+            )}</span>`;
+            const staticText = text.slice(onLetter);
+            textEl.innerHTML = goodText + staticText;
+        } else {
+            // TODO: Stretch Goal::: ADD AUDITORY CLUE
+            console.log('FAILURE');
+            const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
+                0,
+                onLetter
+            )}</span>`;
+            const badText = `<span class="text-danger border-bottom border-danger">${text[onLetter]}</span>`;
+            const staticText = text.slice(onLetter + 1);
+            textEl.innerHTML = goodText + badText + staticText;
+        }
+        console.groupEnd();
     }
-    console.groupEnd();
 });
