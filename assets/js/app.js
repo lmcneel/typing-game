ecee05167e76ee8f996fc2b483df6896370e4d9a
Latisha McNeel <latisha.mcneel@gmail.com>
2020-04-25 18:03:36 -0500

    Text changes colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// BUG: Shift key causes failure....
+const text2 = 'Hello world!';
 const textEl = document.querySelector('#typing-text');
 textEl.textContent = text;
 let onLetter = 0; // TODO: Rename this variable
@@ -10,10 +12,31 @@ document.body.addEventListener('keyup', (event) => {
         console.log('SUCCESS');
         onLetter++;
         // TODO: Progress the visual letter cue
-        //
+        // Change letter to blue
+        // match the border
+        // Change the text from the 1st letter to the current onLetter
+        // ADDING HTML so I need to use the innerHTML function
+        // slice(0, onLetter) -> Should be the part that needs to wrapped
+        // slice(onLetter) -> this should the part that shouldn't be wrapped
+        const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
+            0,
+            onLetter
+        )}</span>`;
+        const staticText = text.slice(onLetter);
+        textEl.innerHTML = goodText + staticText;
     } else {
         // TODO: Indicate to the user that the letter is wrong
+        // TODO: Stretch Goal::: ADD AUDITORY CLUE
+        // change letter to red
+        // match the border
         console.log('FAILURE');
+        const goodText = `<span class="text-primary border-bottom border-primary">${text.slice(
+            0,
+            onLetter
+        )}</span>`;
+        const badText = `<span class="text-danger border-bottom border-danger">${text[onLetter]}</span>`;
+        const staticText = text.slice(onLetter + 1);
+        textEl.innerHTML = goodText + badText + staticText;
     }
     console.groupEnd();
 });
